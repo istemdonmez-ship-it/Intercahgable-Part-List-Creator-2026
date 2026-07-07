93d1d0ea24926b58b7385b9f48b17d6f1b170512
ID <istemdonmez@gmail.com>
2026-07-07 11:32:11 +0300

    Revert "Add project information fields to file upload section"

--- js/app.js
+++ js/app.js
@@ -157,11 +157,6 @@ async function handleAnalyze() {
             model:    f.pumpModel,
             serial:   f.serialNo,
             location: f.location,
-            ksbReferenceNumber: f.ksbReferenceNumber,
-            preparedBy: f.preparedBy,
-            endUserName: f.endUserName,
-            endUserNumber: f.endUserNumber,
-            installationPointNumber: f.installationPointNumber,
         }));
 
         displayResults(analyzedData, interchangeableEnabled);
